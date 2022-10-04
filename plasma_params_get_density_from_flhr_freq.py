f78759739af00cce9da4f9f8b841c86c35833e95
awbrenem <78624201+awbrenem@users.noreply.github.com>
2022-10-04 09:55:36 -0400

    tested and works

--- plasma_params_get_density_from_flhr_freq.py
+++ plasma_params_get_density_from_flhr_freq.py
@@ -4,9 +4,9 @@ Get plasma density from identification of lower hybrid frequency (Currently only
     def flhr_IonMassFractions(ne, fce, nH_ne, nO_ne)
         Lower hybrid frequency (not in high density limit) for fractional percentages of H+ and O+
     def flhr_H(ne, fce, fcH)
-        Lower hybrid frequency (not in high density limit) for full H+
+        Lower hybrid frequency (not in high density limit) for 100% H+
     def flhr_O(ne, fce, fcO)
-        Lower hybrid frequency (not in high density limit) for full O+
+        Lower hybrid frequency (not in high density limit) for 100% O+
 
 All frequencies in Hz, densities in cm-3
 
@@ -20,7 +20,7 @@ me     = 9.1093897e-31
 mp     = 1.6726231e-27
 mH = mp         #H+ mass
 mO = 15.*mp     #O+ mass
-
+Hplus2e_mass = mp/me
 
 
 
@@ -36,36 +36,30 @@ def dens_IonMassFractions(flh, fce, nH_ne, nO_ne):
     den = ((fce**2 * me) * (rH/mH + rO/mO)) / flh**2  
     ne = num/(den - 1)
 
-
-    """
-    fpe = [8980.*np.sqrt(ne[i]) for i in range(len(ne))]
-
-    nH = [nH_ne[i]*ne[i] for i in range(len(ne))] #cm-3 
-    nO = [nO_ne[i]*ne[i] for i in range(len(ne))] #cm-3 
-    
-    Meff = [1./((me/ne[i])* ((nH[i]/mH) + (nO[i]/mO))) for i in range(len(ne))]
-    return [np.sqrt((1./Meff[i])*((fce[i]**2.*fpe[i]**2.)/(fpe[i]**2. + fce[i]**2.))) for i in range(len(fce))] 
-    """
     return ne
 
+
 """
-Lower hybrid frequency (not in high density limit) for full H+
+Lower hybrid frequency (not in high density limit) for 100% H+
 """
 def dens_H(flh, fce, fcH):
 
+    #Version 1 based on reduced form of nO and nH full version
     num = (fce/8980)**2
     den = (me/mH) * (fce**2/flh**2)
     ne = num/(den-1)
 
-    #fpe = [8980.*np.sqrt(ne[i]) for i in range(len(ne))] #Hz
-    #fpH = [fpe[i]/(np.sqrt(1.)*43.) for i in range(len(ne))]
-    #return [np.sqrt(fpH[i]**2*fce[i]*fcH[i])/np.sqrt(fce[i]*fcH[i]+fpH[i]**2) for i in range(len(fce))]
+    ##Version 2 (**Checked - gives same answer as Version 1)
+    #gama = np.sqrt(1)*43
+    #num = fce*fcH*(gama/8980)**2
+    #den = fce*fcH/(flh**2)
+    #ne = num/(den - 1)
 
     return ne 
 
 
 """
-Lower hybrid frequency (not in high density limit) for full O+
+Lower hybrid frequency (not in high density limit) for 100% O+
 """
 def dens_O(flh, fce, fcO):
 
@@ -73,9 +67,12 @@ def dens_O(flh, fce, fcO):
     den = (me/mO) * (fce**2/flh**2)
     ne = num/(den-1)
 
-    #fpe = [8980*np.sqrt(ne[i]) for i in range(len(ne))] #Hz
-    #fpO = [fpe[i]/(np.sqrt(15.)*43.) for i in range(len(ne))]
-    #return [np.sqrt(fpO[i]**2*fce[i]*fcO[i])/np.sqrt(fce[i]*fcO[i]+fpO[i]**2) for i in range(len(fce))]
+    #Version 2 (**Checked - gives same answer as Version 1)
+    gama = np.sqrt(15)*43
+    num = fce*fcO*(gama/8980)**2
+    den = fce*fcO/(flh**2)
+    ne = num/(den - 1)
+
 
     return ne 
 
@@ -83,4 +80,17 @@ def dens_O(flh, fce, fcO):
 
 if __name__ == '__main__': 
     print("Running as script")
+    flh = 1000.
+    fce = 1e6 
+    nH_ne = 0. 
+    nO_ne = 1.
+    ne1 = dens_IonMassFractions(flh, fce, nH_ne, nO_ne)
+
+    fcH = fce/(Hplus2e_mass*1)
+    ne2 = dens_H(flh, fce, fcH)
+
+
+    fcO = fce/(Hplus2e_mass*15)
+    ne3 = dens_O(flh, fce, fcO)
+
 
